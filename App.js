d8af6507262057ccba857d0f6be8425808423a18
Anton Zmanovskii <antonzm34@gmail.com>
2021-02-07 12:07:01 +0300

    try to change styles and emulator install

--- App.js
+++ App.js
@@ -5,8 +5,8 @@ import { StyleSheet, Text, View } from 'react-native';
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <Text style={styles.title}>Hello apps world!</Text>
+      <StatusBar style='auto' />
     </View>
   );
 }
@@ -14,8 +14,13 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    backgroundColor: 'red',
     alignItems: 'center',
     justifyContent: 'center',
+  },
+  title: {
+    fontWeight: 'bold',
+    fontSize: 32,
+    color: 'white',
   },
 });
