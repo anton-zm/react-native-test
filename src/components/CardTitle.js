9ceb0e394e41d6ae5992f85607e8b744b3144bbd
Anton Zmanovskii <antonzm34@gmail.com>
2021-02-07 13:00:27 +0300

    create card-title component

---
+++ src/components/CardTitle.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import { Text, StyleSheet } from 'react-native';
+
+export const CardTitle = (props) => {
+  return <Text style={styles.cardTitle}>{props.title}</Text>;
+};
+
+const styles = StyleSheet.create({
+  cardTitle: {
+    color: 'white',
+    fontSize: 18,
+  },
+});
