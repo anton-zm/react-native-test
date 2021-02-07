0f636544991da199b2b0b52bc26268fd48be13da
Anton Zmanovskii <antonzm34@gmail.com>
2021-02-07 13:16:43 +0300

    create data array

--- App.js
+++ App.js
@@ -1,12 +1,15 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
+import { Card } from './src/components/Card';
 
 export default function App() {
   return (
     <View style={styles.container}>
       <Text style={styles.title}>Hello apps world!</Text>
-      <StatusBar style='auto' />
+      {/* <View>{dataArray.map((item, index) => (
+            <Card  title={item.title}  key={index} text={item.text} />
+          ))}</View> */}
     </View>
   );
 }
