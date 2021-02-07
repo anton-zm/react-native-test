fabc039c05a9cf4e5b77991e61ced40490bebdb7
Anton Zmanovskii <antonzm34@gmail.com>
2021-02-07 13:06:18 +0300

    create card component

---
+++ src/components/Card.js
@@ -0,0 +1,24 @@
+import React from 'react';
+import { Text, StyleSheet, View } from 'react-native';
+import { CardTitle } from './CardTitle';
+
+export const Card = (props) => {
+  return (
+    <View style={styles.card}>
+      <CardTitle title={props.title} />
+      <Text>{props.text}</Text>
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({
+  card: {
+    borderColor: 'white',
+    flexDirection: 'column',
+    borderRadius: 5,
+    padding: 10,
+  },
+  text: {
+    color: 'white',
+  },
+});
